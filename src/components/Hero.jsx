a319b6e8a07c9b26ea94dd88122cb452a11a636a
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-07 11:12:16 +0600

    added slider image from api

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,3 @@
-import heroImg1 from "../assets/images/Rectangle 28.png";
-import heroImg2 from "../assets/images/Rectangle 27.png";
-import heroImg3 from "../assets/images/Rectangle 26.png";
-
 import {
   Carousel,
   CarouselContent,
@@ -10,21 +6,33 @@ import {
   CarouselPrevious,
 } from "./ui/carousel";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
-  const images = [heroImg1, heroImg2, heroImg3];
+  const [travel, setTravel] = useState([]);
+  useEffect(() => {
+    fetch("travel.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setTravel(data);
+      });
+  }, []);
+  console.log(travel[0]);
+  const imageUrls = travel.map((destination) => destination.image_url);
+  console.log(imageUrls);
+
   return (
-    <section className="w-full text-white">
+    <section className="w-full text-white mt-32  lg:mt-10">
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
         <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-            Coxs
+            Cox&apos;s
             <span> bazar</span>
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12">
-            Coxs Bazar is a city, fishing port, tourism centre and district
-            headquarters in southeastern Bangladesh. It is famous mostly for its
-            long natural sandy beach, and it ...
+            Cox&apos;s Bazar is a city, fishing port, tourism centre and
+            district headquarters in southeastern Bangladesh. It is famous
+            mostly for its long natural sandy beach, and it ...
           </p>
           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
             <button className="px-8 text-lg rounded font-bold btn btn-warning dark:bg-violet-600 dark:text-gray-50">
@@ -40,16 +48,20 @@ const Hero = () => {
           className="w-full lg:max-w-3xl"
         >
           <CarouselContent>
-            {images.map((image, index) => (
-              <CarouselItem key={index} className="md:basis-1/2 lg:basis-4/5">
+            {travel.map((destination) => (
+              <CarouselItem
+                key={destination.id}
+                className="md:basis-1/2 lg:basis-4/5"
+              >
                 <div className="p-1">
-                  <Link to={`/destinationDetails${index + 1}`}>
+                  <Link to={`/destinationDetails${destination.id}`}>
                     <img
-                      src={image}
-                      alt={`Hero Image ${index + 1}`}
-                      className="w-full rounded-2xl"
+                      src={destination.image_url}
+                      alt={`Hero Image ${destination.id}`}
+                      className="w-[400px] h-[350px] lg:w-[800px] lg:h-[550px] object-cover rounded-2xl"
                     />
                   </Link>
+                  <span className="text-white relative left-5 bottom-20 font-bebas text-4xl ">{destination.destination_name}</span>
                 </div>
               </CarouselItem>
             ))}
